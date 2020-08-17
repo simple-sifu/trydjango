b5e86c4407878dd80afc2cff493abf802fdaf415
simple-sifu <kattyntommy@gmail.com>
2020-08-17 14:24:28 -0400

    initial version

---
+++ products/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=120)  # max_length = required
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(decimal_places=2, max_digits=10000)
+    summary = models.TextField(blank=True, null=False) # blank is false, required; null is false, database field can be empty.
+    featured = models.BooleanField()  # null=True, default=True
+  
